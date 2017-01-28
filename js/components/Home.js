194ae66cf5737d482dd8774af198503de5167a99
Billy Everyteen <kyle@datascience.com>
2017-01-27 18:23:31 -0800

    test

--- js/components/Home.js
+++ js/components/Home.js
@@ -13,7 +13,7 @@ export default class Home extends React.Component {
 				<div className="config-icon-container"><img src="/public/images/config.png" /></div>
 				<h1>Thinking Cap</h1>
 				<div className="logo-container"><img src="/public/images/logo.png" /></div>
-				<MyButton to="/students">Start</MyButton>
+				<MyButton to="/students">Start!</MyButton>
 			</div>
 		)
 	}
